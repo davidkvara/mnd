f5bbc05f9aab24853334f9895604112fb64d30da
davidkvara <davekvaratskhelia@gmail.com>
2018-12-09 21:24:28 +0400

    project setup

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default () => (
+  <div style={{ textAlign: `center`, fontFamily: `sans-serif` }}>
+    <Head>
+      <title>MND now</title>
+    </Head>
+    <h2>MND now</h2>
+  </div>
+);
